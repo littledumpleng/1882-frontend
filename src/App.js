5d05261eff74629ab2433205d39c889cc0c92903
Dominique Rafael <drafael@terpmail.umd.edu>
2021-07-29 16:40:21 -0400

    login ishhh

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import 'bulma/css/bulma.min.css';
 import './App.css';
-import { BrowserRouter as Router,Route, Switch, Link, Redirect } from 'react-router-dom';
-import {Homepage} from './views/Homepage';
-import {Contact} from './views/Contact';
-import {Featured} from './views/Featured';
-import {About} from './views/About';
-import {Header} from './components/Header';
-import {Footer} from './components/Footer';
-import {Admin} from './views/Admin';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import { Homepage } from './views/Homepage';
+import { Contact } from './views/Contact';
+import { Featured } from './views/Featured';
+import { About } from './views/About';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Login } from './views/Login';
+import { AdminDash } from './views/AdminDash';
 
 const App = () => {
+  const [token, setToken] = useState();
+
   return (
     <div className="App">
       <Header />
@@ -30,10 +34,15 @@ const App = () => {
           <Route exact path="/featured">
             <Featured />
           </Route>
-          <Route exact path="/admin">
-            <Admin />
+          <Route exact path="/login">
+            {/* took out the stuff with if from tutorial */}
+              <Login setToken={setToken} />
+            
           </Route>
-      </div>
+          <Route exact path="/admindash">
+            <AdminDash />
+          </Route>
+        </div>
       </Router>
       <Footer />
     </div>
---
+++ src/views/AdminDash.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const AdminDash = (props) => {
+    return (
+        <h2>Dashboard</h2>
+        )
+    }

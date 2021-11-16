ff1394ad40553c110424b8bbbe0dda40246de4f1
Dominique Rafael <drafael@terpmail.umd.edu>
2021-11-16 15:08:11 -0500

    idk

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import logo from '../images/1882logo.png';
 
+
 export const Header = (props) => {
-    return (
-        <div className="banner">
+  return (
+    <div className="banner">
       <div className="inner-banner">
-        <img
-          src={logo}
-          alt="1882 Foundation Logo"
-          className="has-ratio"
-          width="198"
-        />
-        <span id="title">Literature & Art Corner Database</span>
-        <a href="https://1882foundation.org" id="goto">Go to Home Site</a>
+
+        <a href="https://1882foundation.org">
+          <img
+            src={logo}
+            alt="1882 Foundation Logo"
+            className="has-ratio"
+            width="198"
+          />
+        </a>
+
+        <a href="/home">
+          <span id="title">Literature & Art Corner Database</span>
+        </a>
+        {/* <a href="https://1882foundation.org" id="goto">Go to Home Site</a> */}
       </div>
-      <nav className="navbar red">
+
+      <div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+      
+      {/* <nav className="navbar red">
         <div className="navbar-brand">
           <span className="navbar-burger burger" data-target="navMenu">
             <span></span>
@@ -23,15 +41,15 @@ export const Header = (props) => {
           </span>
         </div>
         <div id="navMenu" className="navbar-menu">
-          <div className="navbar-end">
-            <a href="/home" className="navbar-item">Home</a>
-            <a href="/about" className="navbar-item">About</a>
+          <div className="navbar-end"> */}
+            {/* <a href="/home" className="navbar-item">Home</a> */}
+            {/* <a href="/about" className="navbar-item">About</a>
             <a href="/suggestion" className="navbar-item">Make a Suggestion</a>
             <a href="/featured" className="navbar-item">Featured Reviews</a>
             <a href="/contact" className="navbar-item">Contact Us</a>
           </div>
         </div>
-      </nav>
+      </nav> */}
     </div>
-    )
+  )
 }

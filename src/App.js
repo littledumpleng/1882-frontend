23d95f1d335721055c6884248062a169f8af4da1
Dominique Rafael <drafael@terpmail.umd.edu>
2021-12-28 14:14:17 -0500

    last updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import 'bulma/css/bulma.min.css';
 import './App.css';
-import { BrowserRouter as Router,Route, Switch, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Link, Navigate } from 'react-router-dom';
 import {Homepage} from './views/Homepage';
 import {Contact} from './views/Contact';
 import {Featured} from './views/Featured';
 import {About} from './views/About';
 import {Header} from './components/Header';
 import {Footer} from './components/Footer';
+import { Route } from 'react-router';
+
+// fix it to comply with v6
+// https://dev.to/iamandrewluca/private-route-in-react-router-v6-lg5
 
 const App = () => {
   return (
@@ -14,21 +18,31 @@ const App = () => {
       <Header />
       <Router>
         <div>
+        <Routes>
           <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>s
+            <Navigate to="/home" />
+          </Route>
+          </Routes>
+          <Routes>
           <Route exact path="/home">
             <Homepage />
           </Route>
+          </Routes>
+          <Routes>
           <Route exact path="/contact">
             <Contact />
           </Route>
+          </Routes>
+          <Routes>
           <Route exact path="/about">
             <About />
           </Route>
+          </Routes>
+          <Routes>
           <Route exact path="/featured">
             <Featured />
           </Route>
+          </Routes>
       </div>
       </Router>
       <Footer />
@@ -36,32 +50,4 @@ const App = () => {
   );
 }
 
-// const App = () => {
-//   // by default take us to user accesible pages
-//   // if the admin login button is pressed, take to login page:
-//   // if token is input, take to admin dash
-//   // if token is missing, take to either homepage or error page
-//   // else return homeapp
-
-//   const [token, setToken] = useState();
-
-//   if (!token) { // if there is no token put in, it returns the login page
-//     return <LoginPage setToken={setToken} />
-//   }
-
-//   return <HomeApp /> // returns user accesible pages and admin dashboard
-// }
-
 export default App;
-
-// else if (token) {
-//   return (
-//     <div className="App">
-//     <Router>
-//       <Route exact path="/login">
-//         <AdminDash />
-//       </Route>
-//     </Router>
-//     </div>
-//   )
-// }

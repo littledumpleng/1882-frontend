3f0868d7df34626c0da77f9be8b3a21640d5dd9a
Lotun16 <38048224+Lotun16@users.noreply.github.com>
2022-01-11 21:54:16 -0500

    moved header youtube and filter changes to repo

--- src/components/Backgrounds.js
+++ src/components/Backgrounds.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 
 export const Backgrounds = (props) => {
     const BACKGROUNDS = ['American', 'Chinese', 'Indian', 'Filipino', 'Vietnamese', 'Korean', 'Japanese', 'Pakistani', 'Cambodian', 'Hmong', 'Thai', 'Laotian', 'Bangladeshi', 'Burmese', 'Nepalese', 'Indonesian', 'Sri Lankan', 'Malaysian', 'Bhutanese', 'Mongolian', 'Taiwanese', 'Asian (Unspecified)'];
+    const [selectedBackground, setSelectedBackground] = useState('Select Background');
     return (
-        <div className="box">
+        <div className="box filter-div">
                 <div className="dropdown is-hoverable">
                   <div className="dropdown-trigger">
                     <button
-                      className="button"
+                      className="button filter-but"
                       aria-haspopup="true"
                       aria-controls="background-dropdown"
                     >
-                      <span>Select a Background</span>
+                      <span>{selectedBackground}</span>
                       <span className="icon is-small">
                         <i className="fas fa-angle-down" aria-hidden="true"></i>
                       </span>
@@ -23,7 +25,7 @@ export const Backgrounds = (props) => {
                     role="menu"
                   >
                     <div className="dropdown-content">
-                      {BACKGROUNDS.map(type => (<a href="#" className="dropdown-item">{type}</a>))}
+                      {BACKGROUNDS.map(type => (<a href="#" className="dropdown-item" onClick={(e) => setSelectedBackground(e.target.innerText)}>{type}</a>))}
                     </div>
                   </div>
                 </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faYoutube, faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import logo from '../images/1882logo.png';
 
 export const Header = (props) => {
-  return (
-    <div className="banner">
+  const facebook = <FontAwesomeIcon icon={faFacebookF} size="lg" />
+  const instagram = <FontAwesomeIcon icon={faInstagram} size="lg" />
+  const youtube = <FontAwesomeIcon icon={faYoutube} size="lg" />
+    return (
+        <div className="banner">
       <div className="inner-banner">
-
-        <a href="https://1882foundation.org">
-          <img
-            src={logo}
-            alt="1882 Foundation Logo"
-            className="has-ratio"
-            width="198"
-          />
-        </a>
-
-        <a href="/home">
-          <span id="title">Literature & Art Corner Database</span>
-        </a>
-        {/* <a href="https://1882foundation.org" id="goto">Go to Home Site</a> */}
+        <img
+          src={logo}
+          alt="1882 Foundation Logo"
+          className="has-ratio"
+          width="198"
+        />
+        <span id="title">Literature & Art Corner Database</span>
       </div>
-      
       <nav className="navbar red">
         <div className="navbar-brand">
           <span className="navbar-burger burger" data-target="navMenu">
@@ -31,16 +28,21 @@ export const Header = (props) => {
         </div>
         <div id="navMenu" className="navbar-menu">
           <div className="navbar-end">
-            {/* <a href="/home" className="navbar-item">Home</a> */}
-            <a href="/about" className="navbar-item">About</a>
-            <a href="/about" className="navbar-item">Featured</a>
-            <a href="/about" className="navbar-item">Resources</a>
-            {/* <a href="/suggestion" className="navbar-item">Make a Suggestion</a>
-            <a href="/featured" className="navbar-item">Featured Reviews</a>
-            <a href="/contact" className="navbar-item">Contact Us</a> */}
+            <a href="/home" className="navbar-item">HOME</a>
+            <a href="/about" className="navbar-item">ABOUT</a>
+            <a href="/suggestion" className="navbar-item">SUGGESTIONS</a>
+            <a href="/featured" className="navbar-item">REVIEWS</a>
+            <a href="/contact" className="navbar-item">CONTACT</a>
+            <a href="https://www.facebook.com/1882ProjectFoundation/" className="navbar-item">{facebook}</a>
+            <a href="https://www.instagram.com/1882foundation/?hl=en" className="navbar-item">{instagram}</a>
+            <a href="https://www.youtube.com/channel/UC00NyHsuZW5OpklnE-mLwRw" className="navbar-item">{youtube}</a>
+            <a href="https://1882foundation.org" className='navbar-item'>
+            <button className=" main-btn"><a>MAIN SITE</a></button>
+            </a>
           </div>
         </div>
       </nav>
+      {/* <div className="navbar-second">.hvjvj</div> */}
     </div>
-  )
+    )
 }

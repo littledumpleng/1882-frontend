4e45d1b4b241391b4b466b31b0bdca2ad65d5f40
Lauren Eng <73508837+littledumpleng@users.noreply.github.com>
2022-02-02 10:31:29 -0500

    Added breaks

--- src/components/Review.js
+++ src/components/Review.js
@@ -4,6 +4,7 @@ export const Review = (props) => {
     return ( 
         <div className="content_element">
             <h3>Review of <em>The Refugees</em></h3>
+            <br/>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
         </div>
     )

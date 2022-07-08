b184180eedc9b89674bc49382bc03a48dd064cff
acuteengle <bce25@cornell.edu>
2022-07-07 21:29:13 -1000

    admin login works

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -3,3 +3,5 @@ export const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:9000/a
 export const DEFAULT_AXIOS_OPTIONS = {
   withCredentials: true,
 }
+
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "admin";
--- src/views/AdminLogin.js
+++ src/views/AdminLogin.js
@@ -1,26 +1,44 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { ADMIN_PASSWORD } from '../AppConstants';
 
 export const AdminLogin = () => {
+  const navigate = useNavigate();
   const [password, setPassword] = useState();
+  const [responseMessage, setResponseMessage] = useState(null);
 
-  const onSubmitClick = () => {
-    // check if the password matches
-    // then set sessionStorage
-    // redirect to AdminDashboard
+  const onLoginClick = () => {
+    if (password === ADMIN_PASSWORD) {
+      setResponseMessage('Success');
+      navigate('/admin');
+    }
+    else {
+      setResponseMessage('Incorrect');
+    }
   }
 
   return (
     <div>
       <h1>Login to your admin</h1>
       <div className="content_element">
-        <input
-          type="password"
-          placeholder='password'
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-        />
-        <br />
-        <button onClick={onSubmitClick}>Submit</button>
+        <div className="column is-one-third">
+          <input
+            className="input"
+            type="password"
+            placeholder='password'
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+        </div>
+        <div className="column">
+          <button
+            className="button is-info"
+            onClick={onLoginClick}
+          >
+            Login
+          </button>
+        </div>
+        <p>{responseMessage ?? ''}</p>
       </div >
     </div>
   )

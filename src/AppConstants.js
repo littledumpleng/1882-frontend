96391856bdac9e6959604060bc3ae9749cb84f64
acuteengle <bce25@cornell.edu>
2022-07-15 11:00:32 -1000

    environment variables

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -1,11 +1,11 @@
-export const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:9000/api";
+export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL || "http://localhost:9000/api";
 
 export const DEFAULT_AXIOS_OPTIONS = {
   withCredentials: true,
 }
 
-export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "admin";
+export const ADMIN_PASSWORD = process.env.REACT_APP_ADMIN_PASSWORD || "admin";
 
-export const ADMIN_LOGIN_SESSION_STORAGE_KEY = process.env.ADMIN_LOGIN_SESSION_STORAGE_KEY || "LWL53LbiVX3rcx3g";
+export const ADMIN_LOGIN_SESSION_STORAGE_KEY = process.env.REACT_APP_ADMIN_LOGIN_SESSION_STORAGE_KEY || "LWL53LbiVX3rcx3g";
 
-export const ADMIN_LOGIN_SESSION_STORAGE_VALUE = process.env.ADMIN_LOGIN_SESSION_STORAGE_VALUE || "CLmZb4krKznWv3ui";
+export const ADMIN_LOGIN_SESSION_STORAGE_VALUE = process.env.REACT_APP_ADMIN_LOGIN_SESSION_STORAGE_VALUE || "CLmZb4krKznWv3ui";
